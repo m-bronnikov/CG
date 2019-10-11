c77bc11699afee75e4a674d4532afac1fd47ea4f
Bronnikoff <max120199@gmail.com>
2019-10-11 23:30:16 +0300

    comments

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -5,34 +5,50 @@
 
 #include <GLFW/glfw3.h>
 
-#include <SOIL/SOIL.h>
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <cmath>
-
 #include "Shader.h"
 #include "multyplyes.h"
 
+// compile: g++ main.cpp -o hello -lGL -lGLEW -lglfw
+// you need glm, glew and glfw libs
+
+// Number of angles in bottom of piramid
 #define NUMBER_OF_LINES 4
+
 #define PIRAMID_HEIGHT 0.7f
 
+// for key press registration:
 bool keys[1024];
 
+// for returning configuration in start state
 bool ret = false;
+
+// for changing scale and rotation in a moment
 GLfloat x_rotation = 0.0f, y_rotation = 0.0f, z_rotation = 0.0f;
 GLfloat scale = 1.0f;
 
+// function of init window and opengl 
 GLFWwindow* InitOpenGl(GLuint width, GLuint height);
+
 void DestroyOpenGl(GLuint VAO, GLuint VBO, GLuint EBO);
+
+// initialisation of buffers and creation of figure
 void InitBuffers(GLuint& VAO, GLuint& VBO, GLuint& EBO, GLuint num_of_angles);
+
+// main loop of drawing
 void GameLoop(GLFWwindow* window, Shader& shader, GLuint VAO, GLuint VBO, GLuint EBO);
+
+// functions  for work with keybord
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 void do_movement();
+
+// func for work with screen size changing
 void new_func_size_callback(GLFWwindow* window, int width, int height);
 
-
+// generation of vector of vertexes of piramid
 GLfloat* vertex_vector(GLuint num_of_angles);
 // void vertex_indexses(GLuint* indexis);
 
@@ -73,6 +89,8 @@ void DestroyOpenGl(GLuint VAO, GLuint VBO, GLuint EBO){
     glfwTerminate();
 }
 
+
+// dont use
 void print_vec(GLfloat* vertex){
     int j = 0;
     for(int i = 0; i < 36*NUMBER_OF_LINES; ++i){
@@ -83,6 +101,7 @@ void print_vec(GLfloat* vertex){
         }
     }
 }
+
 
 void InitBuffers(GLuint& VAO, GLuint& VBO, GLuint& EBO, GLuint num_of_angles){
     GLfloat *vertices = vertex_vector(num_of_angles);
@@ -102,9 +121,11 @@ void InitBuffers(GLuint& VAO, GLuint& VBO, GLuint& EBO, GLuint num_of_angles){
     //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     EBO = 0;
 
+    // first 3 coords - position
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
 
+    // second 3 coord - color
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
 
@@ -205,7 +226,11 @@ void new_func_size_callback(GLFWwindow* window, int width, int height){
 }
 
 void GameLoop(GLFWwindow* window, Shader& shader, GLuint VAO, GLuint VBO, GLuint EBO){
+    
+    // model matrix
     glm::mat4 model = glm::mat4(1.0f);
+
+    // start config of prog:
     const glm::mat4 first_conf = glm::mat4(1.0f);
     while (!glfwWindowShouldClose(window))
     {
@@ -218,34 +243,46 @@ void GameLoop(GLFWwindow* window, Shader& shader, GLuint VAO, GLuint VBO, GLuint
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        // run shaders:
         shader.Use();
 
         glm::mat4 view;
         glm::mat4 projection;
         glm::mat4 rotation;
+
+        // we use quaternions for rotate, but i think we can use standart rotation matrix.
         glm::vec4 quaternion;
         xyz_rotation_to_quaternion(quaternion, x_rotation, y_rotation, z_rotation);
+        // get rotation matrix from quaternion:
         quaternion_to_matrix(rotation, quaternion);
+
+        // scale change matrix
         glm::mat4 scalem = glm::scale(glm::mat4(1.0f), glm::vec3(scale, scale, scale));
+        // returning in start configuration of piramid
         if(ret){
             model = first_conf;
             ret = false;
         }else{
+            // getting new model matrix from old with rotation and scale matrix:
             model = rotation * scalem * model;
         }
+
+        //standart view and projection matrix:
         view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f));
         projection = glm::perspective(glm::radians(45.0f), (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+
+        // finally matrix for multyply with vertex:
         glm::mat4 MVP = projection * view * model;
 
+        // working with shaders
         GLint modelLoc = glGetUniformLocation(shader.Program, "MVP");
-
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(MVP));
-
         glBindVertexArray(VAO);
-
         if(EBO){
             glDrawElements(GL_TRIANGLES, 2*3*NUMBER_OF_LINES, GL_UNSIGNED_INT, 0);
         }else{
+            // now we don't use EBO  and draw triangles these method
             glDrawArrays(GL_TRIANGLES, 0, 2*3*NUMBER_OF_LINES);
         }
         glBindVertexArray(0);
@@ -308,6 +345,7 @@ int main()
 {
     GLFWwindow* window = InitOpenGl(WIDTH, HEIGHT);
 
+    // get and compile shader programs:
     Shader ourShader("shaders/shader.vs", "shaders/shader.frag");
 
     GLuint VBO, VAO, EBO;
